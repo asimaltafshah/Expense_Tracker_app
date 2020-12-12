d78b22b1d782cf81c86983c7ba7ecd4bc115c58d
Asim Altaf Shah <asimaltafshah@gmail.com>
2020-12-12 21:03:00 +0500

    edit Transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -6,7 +6,7 @@ export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
   const sign = transaction.amount < 0 ? "-" : "+";
   return (
-    <li className={sign == "-" ? "minus" : "plus"}>
+    <li className={sign === "-" ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
         {sign}${Math.abs(transaction.amount)}
